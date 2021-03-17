ba5a88170481ce231fade9532c1bbc9f97cc2ecf
Vivienne <43151488+people-r-strange@users.noreply.github.com>
2021-03-17 09:58:01 -0400

    Trying to write function that will transform dataframe into correct dimensions for PLS model.
    Things to Check:
    -are comments correct?
    -can we have functions within the function?
    -don't know how to view the transformed dataframe in the environment that the function should be transforming

---
+++ Transformation_function.R
@@ -0,0 +1,52 @@
+library(tidyverse)
+
+#reading in list of txt files and converting it to a df 
+
+## read in txt files automatically 
+fname <- list.files("OPUS", full.names = T)
+
+## creates list of txt files 
+filelist <- lapply(fname, read.delim, header = F)
+
+##Adding sample IDs (reference to lake core)
+names(filelist) <- gsub(".*/(.*)\\..*", "\\1", fname)
+
+--------------------------------------------------------------------------------------------------------------------------
+#Create function that transforms the df into correct format 
+
+#assign name to our function, input df we want to function to work on
+transform_df <- function(filelist) {
+  
+  #transforming dimensions of df from two columns to on long row (wavenumber becomes column header)
+  filelist[[1]] %>% pivot_wider(names_from = V1, values_from = V2) %>% dim()
+  filelist[[1]] %>% pivot_wider(names_from = V1, values_from = V2) %>% View()
+  
+  #save the transformed df to a new list of df called reformattedData
+  reformattedData <- lapply(filelist, function(x){pivot_wider(x, names_from = V1, values_from = V2)})
+  
+  #Unlist the reformattedData list into ?   
+  allNames <- lapply(reformattedData, names) %>% unlist()
+  
+  #create matrix with sample names in rows and wavenumber in columns, data in cells is absorbance values
+  allNames = do.call("rbind",lapply(reformattedData, names))
+  
+  #manually renaming wavenumber in column headers with "Vi"
+  dropNames <- function(data){
+    names(data)=paste("V", 1:ncol(data), sep="")
+    return(data)
+  }
+  
+  #creating new list of df where there aren't any wavenumbers...only absorbance values 
+  reformattedData2 <- lapply(reformattedData, dropNames)
+  
+  #write over previous ? "allNames" so that the new ? "allNames" does not have wavenumber; only contains absorbance 
+  allNames <- lapply(reformattedData2, names) %>% unlist()
+  
+  ## need to resolve mismatch in wavenumbers before moving forward
+  newData <- do.call(rbind.data.frame,reformattedData2)
+  
+  ## removes sample IDs
+  newData$dataset = names(filelist)
+  
+  return(newData)
+}
