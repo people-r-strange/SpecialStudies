07840ba8b0977bb947e0ac87a7992f0838bf0256
Vivienne <43151488+people-r-strange@users.noreply.github.com>
2021-04-14 10:02:32 -0400

    Trying to extract useful attributes from list but got confused...

--- Trying_PLS.R
+++ Trying_PLS.R
@@ -6,24 +6,30 @@ library(tidyverse)
 dataForProofOfConcept <- read_csv("dataForProofOfConcept.csv")
 View(dataForProofOfConcept)
 
+#Read in NICE dataset
+dataForProofOfConceptNice <- read_csv("dataForProofOfConceptNice.csv")
+View(dataForProofOfConceptNice)
+
+
+
 #Checking column names
-names(dataForProofOfConcept[(ncol(dataForProofOfConcept)-5):ncol(dataForProofOfConcept)])
+#names(dataForProofOfConcept[(ncol(dataForProofOfConcept)-5):ncol(dataForProofOfConcept)])
 
 #Checking position of undesired columns
-which(names(dataForProofOfConcept)== "dataset")
-which(names(dataForProofOfConcept)== "dataset_adjusted")
-which(names(dataForProofOfConcept)== "Sample.y")
-which(names(dataForProofOfConcept)== "Sample.x")
+#which(names(dataForProofOfConcept)== "dataset")
+#which(names(dataForProofOfConcept)== "dataset_adjusted")
+# which(names(dataForProofOfConcept)== "Sample.y")
+# which(names(dataForProofOfConcept)== "Sample.x")
 
 #remove undesired columns
-dataForProofOfConcept <- dataForProofOfConcept %>%
-  select(-1,-(3699:3701))
+# dataForProofOfConcept <- dataForProofOfConcept %>%
+#   select(-1,-(3699:3701))
 
 #Check that undesired columns are removed
-names(dataForProofOfConcept[(ncol(dataForProofOfConcept)-5):ncol(dataForProofOfConcept)])
+# names(dataForProofOfConcept[(ncol(dataForProofOfConcept)-5):ncol(dataForProofOfConcept)])
 
-#PLS
-pls1 <- plsr(BSiPercent~., ncomp = 10, data=dataForProofOfConcept, validation = "LOO")
+#PLS validation = "LOO" 
+pls1 <- plsr(BSiPercent~., ncomp = 10, data=dataForProofOfConceptNice, validation = "LOO")
 
 summary(pls1)
 
@@ -31,4 +37,20 @@ plot(RMSEP(pls1), legendpos = 'topright')
 
 summary(wet_chem_data)
 
-x <-  RMSEP(pls1)
+#List of all attributes 
+PLS_list <-  RMSEP(pls1)
+
+#What we're working with 
+names(pls1)
+
+#SlotNames 
+#slotNames(pls1)
+
+#What we're working with 
+#names(PLS_list)
+
+#Select only relevant attributes 
+
+pls_useful <- pls1 %>%
+  select("coefficients", "scores", "loadings", )
+
