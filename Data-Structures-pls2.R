4db6fd7f5a9c8421a75c4198f318ca3033a029e4
Vivienne <43151488+people-r-strange@users.noreply.github.com>
2021-04-21 13:30:05 -0400

    Also added title and labels to ggplot for pls2

--- Data-Structures-pls2.R
+++ Data-Structures-pls2.R
@@ -8,3 +8,15 @@ dim(data) ## 39 3698
 ### PLS2 ###
 pls2 <- plsr(BSiPercent~., ncomp = 10, data=data, validation = "CV", segments = 5)
 summary(pls2)
+
+plot(RMSEP(pls2))
+
+## save root mean squared error of prediction to object res
+res2 <- RMSEP(pls2)
+
+data <- cbind.data.frame(cv = res2$val[1,,], ncomps = 0:10)
+ggplot(data, aes(ncomps, cv))+geom_point()+geom_line()+
+  labs(title = "RMSEP pls2", 
+       subtitle="CV= 5-fold",
+       y = "CV", 
+       x = "Components") 
