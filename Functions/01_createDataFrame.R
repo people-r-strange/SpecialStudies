b57ad0b10f8d1b20ecf765befbb6b439cc77e536
Vivienne <43151488+people-r-strange@users.noreply.github.com>
2021-08-09 14:04:25 -0400

    function to create dataframe ready to be inputted into pls model

--- Functions/01_createDataFrame.R
+++ Functions/01_createDataFrame.R
@@ -68,7 +68,59 @@ fname <- list.files("OPUS", full.names = T)
     }
   
   
- #output <- transform_df(filelist)
+ output <- transform_df(filelist)
+  
+  #write csv files 
+  write.csv(output$newData, "transformedData.csv", row.names = F)
+  write.csv(output$waveNumberInfo, "waveNumberInfo.csv", row.names = F)
+  
+------------------------------------------------------------------
+    ##FUNCTION 4: Add Calibration Data 
+    
+    #Read in calibration csv with same number of samples as our transformedData 
+    wet_chem_data <- read_csv("csvFiles/resolved-sample-name.csv")
+  
+    #Read in absorbance values for each sample
+    transformedData <- read_csv("csvFiles/transformedData.csv")
+    
+    addWetChem <- function(transformedData) {
+      ## Check if sample names are present 
+      names(transformedData)[ncol(transformedData)] 
+      
+      ## rearranging so dataset id is first, should do this in the transformating function instead moving forward
+      transformedData <- cbind.data.frame(dataset = transformedData$dataset, transformedData[,-ncol(transformedData)]) 
+      
+      #Rename wet_chem_data columns 
+      names(wet_chem_data)[1] <- "dataset"
+      names(wet_chem_data)[2] <- "BSiPercent"
+      
+      #bind calibration data to transformed data
+      Complete_data <- full_join(wet_chem_data, transformedData, by = "dataset")
+      
+      ## this replaces .0 with a space, the backslashes escape the special character . in regular expressions
+      Complete_data$dataset = gsub("\\.0","",Complete_data$dataset) 
+      
+      ## this replaces cm with a space, the backslashes escape the special character . in regular expressions
+      Complete_data$dataset = gsub("cm","",Complete_data$dataset)
+      
+      return(Complete_data)
+    }
+    
+    Data <- addWetChem(transformedData)
+    
+    #Write csv file 
+    write.csv(Data,"csvFiles/resolvedSampleNames-2.csv",row.names=F)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
   -----------------------------------------------------------
   ## Big Function
   runAll <- function(fname) {
