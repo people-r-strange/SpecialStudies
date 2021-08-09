f92ab4fd80c758caf70c8ce2915861e92c5ec8ff
Vivienne <43151488+people-r-strange@users.noreply.github.com>
2021-08-09 15:36:39 -0400

    Run PLS model + Loading Plots

--- Functions/02_createModel.R
+++ Functions/02_createModel.R
@@ -1,23 +1,25 @@
-#Load relevant libraries
+#Load relevant libraries----
 library(pls)
 library(tidyverse)
 library(scales)
 library(readr)
-------------------------------------------------
+#Load Absorbance Data----
   #Read in data containing wet chem BSi percentages 
-  data <- read_csv("csvFiles/resolvedSampleNames-2.csv")
-  dim(data) ## 28 3699
+  data <- read_csv("csvFiles/resolvedSampleNames-2.csv") ### 28 3699
+  data <- data %>%
+    select(-1)
+  dim(data) ## 28 3698
   
-------------------------------------------------
-  #Code for PLS model
+#Code for PLS model----
+  
     ##Select CV based on number of samples
     
     ### Cross-Validation is 5-fold ###
     pls2 <- plsr(BSiPercent~., ncomp = 10, data=data, validation = "CV", segments = 5)
     ### Summary 
     print(summary(pls2))
-------------------------------------------------
-  #Root Mean Squared Error Plots
+#Root Mean Squared Error Plots----
+  
       #Plot RMSEP
       plot(RMSEP(pls2))
     
@@ -36,18 +38,43 @@ library(readr)
              x = "Number of Components") +
         scale_x_continuous(breaks = c(1:10)) + 
         theme_minimal()
-----------------------------------------------------
-  #Create "test data" with different loadings 
-        ## Load wavenumber data 
-        waveNumberInfo <- read_csv("csvFiles/waveNumberInfo.csv") ## 28 3698
+#Create "test data" with different loadings ----
+  
+    ## Load Opus txt file 
+       # reading in list of txt files and converting it to a df
         
-        wavenumber <- as.numeric(as.vector(unname(t(waveNumberInfo[1,]))))
+      ## read in txt files automatically
+      fname <- list.files("OPUS", full.names = T)
       
+      ##FUNCTION 1: Add Sample Names
+      addSampleNames <- function(fname) {
+        ## creates list of txt files
+        filelist <- lapply(fname, read.delim, header = F)
+        
+        ## Adding sample IDs (reference to lake core)
+        names(filelist) <- gsub(".*/(.*)\\..*", "\\1", fname)
+        
+        return(filelist)
+      }
       
-      #binding the two
-      #cross_validated_errors <- cbind.data.frame(cv = res2$val[1,,], ncomps = 0:10)
+      #Save list of txt files with sample names
+      filelist <- addSampleNames(fname)
       
-      #Data frame for different loadings 
+      # save the transformed df to a new list of df called reformattedData
+      reformattedData <- lapply(filelist, function(x) {
+        pivot_wider(x, names_from = V1, values_from = V2)
+      })
+      
+      # Unlist the reformattedData list into matrix
+      allNames <- lapply(reformattedData, names) 
+      
+      # convert matrix into dataframe
+      allNames2 <- as.data.frame(do.call("rbind",allNames))
+      
+      wavenumber <- as.numeric(as.vector(unname(t(allNames2[1,])))) ### 1 3697
+  
+      
+#Data frame for different loadings
       #loadings, #first column of random opus txt file)
       testdata<- cbind.data.frame(loadings_1 = pls2$loadings[,1], 
                                   loadings_2= pls2$loadings[,2], 
@@ -57,8 +84,8 @@ library(readr)
                                   weighted_loading_3 = pls2$loading.weights[,3], 
                                   wavenumber = wavenumber)
       
-------------------------------------------------------
-  #Loading Plots
+#Loading Plots----
+ 
          #Loading plot for Full Spectrum loading 1 with legend and vertical lines
          
          ggplot(testdata, aes(x=wavenumber)) + 
